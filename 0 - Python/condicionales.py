0d90f00cbddd5935cd9579b577eec5b579b98c71
Marco Cordero <marcko_385@hotmail.com>
2024-01-26 07:19:11 -0600

    Inicial

---
+++ 0 - Python/condicionales.py
@@ -0,0 +1,25 @@
+# Comentario de sola línea
+'''
+    Comentario
+    de 
+    varias líneas
+'''
+
+x = True
+y = False
+a = 10
+b = 10000
+
+# IF
+if (x != y):
+    print('Distinto')
+
+    if (a > b):
+        pass
+    elif (x):
+        print(x)
+    else:
+        print('No coincidió con nada')
+
+var = 50 if a > 5 else 20
+print(var)
